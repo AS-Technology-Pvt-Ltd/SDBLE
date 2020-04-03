3dc20088da7cb5a2e9b863d82a406223cd6cf6a4
Sachin Gupta <sachin.gupta@tutree.com>
2020-04-03 18:15:13 +0530

    calculation for done for distance

--- src/Demo.js
+++ src/Demo.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {BleManager} from 'react-native-ble-plx';
 
 class Demo extends PureComponent {
@@ -20,7 +20,50 @@ class Demo extends PureComponent {
       }
     }, true);
   }
-  scanAndConnect() {
+
+  calculateDistance = (rssi, txPower) => {
+    var txPower = -59; //hard coded power value. Usually ranges between -59 to -65
+    // console.log(rssi, txPower);
+
+    if (rssi == 0) {
+      return -1.0;
+    }
+
+    var ratio = (rssi * 1.0) / txPower;
+    if (ratio < 1.0) {
+      return Math.pow(ratio, 10).toFixed(2);
+    } else {
+      var distance = 0.89976 * Math.pow(ratio, 7.7095) + 0.111;
+      return distance.toFixed(2);
+    }
+
+    /*
+
+    1. Reference 
+
+@source :https://forums.estimote.com/t/determine-accurate-distance-of-signal/2858/2
+
+One of the simplest formulas for calculating distance (found it in this paper 2.0k) is:
+
+RSSI = -20 * logd + TxPower (where d = distance)
+
+Which gives you this:
+
+d = 10 ^ ((TxPower - RSSI) / 20)
+
+This will give you very “jumping” results, coming from the nature of RSSI. 
+RSSI readings are not very stable and highly depend on environment. 
+I’m pretty sure Estimotes uses much mores sophisticated formula that also enables some kind of “smoothing” of data, 
+so the readings are more stable. But this will be a good start to get at least some approximation.
+
+
+
+2. Reference 
+
+Source :https://www.radiusnetworks.com/2018-11-19-fundamentals-of-beacon-ranging
+    */
+  };
+  scanAndConnect = () => {
     const {bleManager} = this.state;
     bleManager.startDeviceScan(null, null, (error, device) => {
       if (error) {
@@ -29,14 +72,6 @@ class Demo extends PureComponent {
         return;
       }
       if (device) {
-        console.log(Object.keys(device));
-
-        /*
-
-["serviceUUIDs", "txPowerLevel", "overflowServiceUUIDs", "serviceData", "isConnectable", "id", "localName", "manufacturerData", "rssi", "mtu", "name", "solicitedServiceUUIDs", "_manager"]
-
-        */
-
         this.setState({
           deviceList: [
             ...this.state.deviceList,
@@ -53,30 +88,45 @@ class Demo extends PureComponent {
       //   setTimeout(() => {
       //     bleManager.stopDeviceScan();
       //   }, 2000);
-      bleManager.stopDeviceScan();
+      // bleManager.stopDeviceScan();
     });
-  }
+  };
+
+  uniqueBy = prop => list => {
+    const uniques = {};
+    return list.reduce((result, item) => {
+      if (uniques[item[prop]]) return result;
+      uniques[item[prop]] = item;
+      return [...result, item];
+    }, []);
+  };
 
   render() {
     const {deviceList} = this.state;
+    const uniqueById = this.uniqueBy('localName');
+    let uDevice = deviceList.length ? uniqueById(deviceList) : [];
+    console.log(uDevice);
+
     return (
       <View>
-        <Text style={{fontSize: 28, textAlign: 'center', marginVertical: 5}}>
-          List of Device
-        </Text>
-        {deviceList.length
-          ? deviceList
+        <Text style={styles.heading}>List of Device</Text>
+        {uDevice.length
+          ? uDevice
               .filter(item => item.localName !== null)
               .map((item, i) => (
                 <View key={i}>
-                  <Text key={i} style={{fontWeight: '600', marginVertical: 2}}>
+                  <Text key={i} style={styles.title}>
                     {i + 1} . {item.localName}
                   </Text>
-                  <View style={{marginLeft: 10}}>
+                  <View style={styles.data}>
                     <Text>UUID:{item.uuid}</Text>
                     <Text>RSSI : {item.rssi}</Text>
                     <Text>
                       TxPowerLevel:{item.txPowerLevel || 'Not Available'}
+                    </Text>
+                    <Text>
+                      Distance(meters):
+                      {this.calculateDistance(item.rssi, item.txPowerLevel)}
                     </Text>
                   </View>
                 </View>
@@ -86,5 +136,20 @@ class Demo extends PureComponent {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: '600',
+    marginVertical: 2,
+  },
+  data: {
+    marginLeft: 10,
+  },
+  heading: {
+    fontSize: 28,
+    textAlign: 'center',
+    marginVertical: 5,
+  },
+});
 
 export default Demo;
